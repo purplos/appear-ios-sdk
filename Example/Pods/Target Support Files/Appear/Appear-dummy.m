a451d848ed0e71e228fb985b95e2b9b1343e5d6a
Magnus Tviberg <hei@purpl.no>
2019-05-01 20:13:17 +0200

    Initial commit🎉

---
+++ Example/Pods/Target Support Files/Appear/Appear-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Appear : NSObject
+@end
+@implementation PodsDummy_Appear
+@end
---
+++ Example/Pods/Target Support Files/Pods-Appear_Tests/Pods-Appear_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Appear_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Appear_Tests
+@end
